19d4e8653a934a2d96683bc52dd1e5e4ce370fee
Nicholas Madore <mado0040@algonquinlive.com>
2021-09-21 16:46:19 -0400

    Sixth Commit - Week 2 (Lab 2): - Added functionality to the green button to show edit text. - Added the ability to change the text above the green button by entering the new text on the "I'm an EditText" line, then click to button to activate activate the text change on-top of the green button. - Added a Switch, RadioButton, and CheckBox and all 3 have Toast messages accordingly. - Added an Algonquin College logo image. - Added an Algonquin College logo image button, with a toast message for the height and width of the image button.

--- app/src/main/java/algonquin/cst2335/mado0040/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/mado0040/MainActivity.java
@@ -3,6 +3,16 @@ package algonquin.cst2335.mado0040;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView mytext=findViewById(R.id.textview);
+        Button mybutton=findViewById(R.id.mybutton);
+        EditText myedit = findViewById(R.id.myedittext);
+        mybutton.setOnClickListener(    (View v) -> {  mytext.setText("Your edit text has: " + myedit.getText().toString());  }   );
+        CheckBox mycheckbox=findViewById(R.id.mycheckbox);
+        Switch myswitch=findViewById(R.id.myswitch);
+        RadioButton myradiobutton=findViewById(R.id.myradiobutton);
+        ImageView myimage=findViewById(R.id.logo_algonquin);
+        ImageButton imgbtn=findViewById(R.id.myimagebutton);
+
+        // Toast message with height and width for ImageButton
+        imgbtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View myimagebutton) {
+                Toast.makeText(getApplicationContext(),
+                        "The width=" + imgbtn.getMeasuredWidth() + " and height=" + imgbtn.getMeasuredHeight() , Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        // Toast message for CheckBox
+        mycheckbox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View mycheckbox) {
+                Toast.makeText(getApplicationContext(),
+                        "It is clear outside today!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        // Toast message for Switch
+        myswitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View myswitch) {
+                Toast.makeText(getApplicationContext(),
+                        "You are a gamer!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        // Toast message for RadioButton
+        myradiobutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View myradiobutton) {
+                Toast.makeText(getApplicationContext(),
+                        "You have seen the rain!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
